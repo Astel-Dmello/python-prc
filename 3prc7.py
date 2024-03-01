178939ac1d4f5263d5ccb5203940c650d7ae287a
Astel-Dmello <144018585+Astel-Dmello@users.noreply.github.com>
2024-03-01 09:28:03 +0530

    Python Practical

---
+++ 3prc7.py
@@ -0,0 +1,4 @@
+num=input("Enter a number:")
+numTup=tuple(num)
+numWords={1:"One",2:"Two",3:"Three",4:"Four"}
+print(numWords[numTup])
---
+++ pr7.py
@@ -0,0 +1,5 @@
+t=(1,2,3)
+minimum=min(t)
+print(minimum)
+maximum=max(t)
+print(maximum) 
